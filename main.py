8e4f5caab851c2c2e24ca94a404c514e1fd21d84
quang <quang99nhv@gmail.com>
2022-03-31 22:18:06 +0700

    Create owner package

---
+++ main.py
@@ -0,0 +1,7 @@
+from my_module.sum import *
+
+a=sum2num(3,4)
+print(a)
+print(NAME)
+say_hello()
+say_bye()
---
+++ my_module/sum.py
@@ -0,0 +1,8 @@
+NAME='YOUR NAME'
+def sum2num(a,b):
+    return a+b
+def say_hello():
+    print("Say hello")
+def say_bye():
+    print("Say bye")
+
